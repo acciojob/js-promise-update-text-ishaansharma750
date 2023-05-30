6cc03dcfac5f766d2a87380c4abbc39fe51dbb77
Suansh Budhiraja <43946155+ishaansharma750@users.noreply.github.com>
2023-05-30 18:15:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function delayedHello() {
+   return new Promise((resolve, reject) => {
+     setTimeout(() => {
+       resolve("Hello, world!");
+     }, 1000);
+   });
+}
+ const outputElement = document.getElementById("output");
+ delayedHello().then((result) => {
+   outputElement.innerText = result;
+ });
